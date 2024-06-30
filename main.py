ce6e36e2f080402716d7391565723233696fdd88
omar Mo <111914594+omar99952@users.noreply.github.com>
2024-06-30 15:00:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ async def create_order(request: Request):
         ,
             "redirect_urls": {
                 "return_url": f"{BASE_URL}/execute-payment",
-                "cancel_url": f"{BASE_URL}
+                "cancel_url": f"{BASE_URL}"
             },
             "application_context": {
                 "brand_name": "ASAS For Furniture",
